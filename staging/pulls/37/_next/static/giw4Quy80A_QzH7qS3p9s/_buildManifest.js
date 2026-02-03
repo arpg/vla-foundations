8f9c6e94695fadc1c4e618d1f1abf9db25855f20
cKohl10 <cKohl10@users.noreply.github.com>
2026-02-03 20:31:21 +0000

    Deploying to gh-pages from @ arpg/vla-foundations@0f8db0b50dddb6a0b85b2a7b879edd1b20313e58 🚀

---
+++ staging/pulls/37/_next/static/giw4Quy80A_QzH7qS3p9s/_buildManifest.js
@@ -0,0 +1,15 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/staging/pulls/37/api/:path*"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
